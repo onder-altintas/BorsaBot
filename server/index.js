e7eb6e331ce8a8f564b39c36c69c9602e6e39cba
Onder Altintas <onderaltintas@gmail.com>
2026-02-17 11:39:38 +0300

    Relax CORS to fix frontend-backend connectivity issue

--- server/index.js
+++ server/index.js
@@ -6,20 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    process.env.FRONTEND_URL
-].filter(Boolean);
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+app.use(cors()); // Allow all origins for easier setup
 app.use(express.json());
 
 // Database setup (simple JSON file for persistence)
